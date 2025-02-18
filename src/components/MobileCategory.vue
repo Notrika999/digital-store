d39318ecbca824d6243447bd2b1f92603ad6ca07
omid haghei <omid.haghei@gmail.com>
2025-02-18 12:12:38 +0330

    Update MobileCategory

--- src/components/MobileCategory.vue
+++ src/components/MobileCategory.vue
@@ -6,9 +6,11 @@ defineEmits(['close']);
 
 <template>
   <div
-    class="fixed inset-0 z-50 border border-l bg-white duration-200"
+    class="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-end"
   >
-      <div class="sidbar-menu mt-3 px-2">
+      <div class="sidbar-menu mt-3 px-2 bg-white w-full rounded-t-lg p-4">
+        <button @click="$emit('close')" class="text-gray-500 text-xl float-left">✖</button>
+
         <Categoray @close="$emit('close')" />
       </div>
 
